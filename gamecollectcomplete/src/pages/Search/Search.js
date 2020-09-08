523b8185a563928540ee869e9168ad96f6a44869
Kuroneko-Rares <luigi.galvan@berkeley.edu>
2020-09-07 19:43:42 -0700

    More search functionality added

--- gamecollectcomplete/src/pages/Search/Search.js
+++ gamecollectcomplete/src/pages/Search/Search.js
@@ -1,26 +1,72 @@
 import React, { useEffect, useState } from "react";
 import "./style.css"
+import API from "../../utils/API"
+import { List, ListItem } from "../../components/List"
 
-function Search(){
+function Search() {
 
     const [game, setGame] = useState("")
+    const [gamelist, setGamelist] = useState([])
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        alert(`Searched game ${game}`)
+        if (!game) {
+            return;
+        }
+        API.searchGame(game)
+            .then(games => {
+                if (games.data.length === 0) {
+                    alert("No results found.")
+                    throw new Error("No results found.");
+                }
+                if (games.data.status === "error") {
+                    throw new Error(games.data.message);
+                }
+                setGamelist(games.data)
+            })
+            .catch(err => console.log(err))
+        
     }
+
+    useEffect(() => {
+
+        gamelist.forEach(game =>{
+
+            API.findGameCover(game.cover)
+                .then(res => game.cover = res)
+
+        })
+
+    }, [gamelist])
+
 
     return (
 
         <div>
             <form onSubmit={handleSubmit}>
                 <label className="searchLabel">
-                Search for a Game: 
-                <input className="searchInput" type="text" value = {game} onChange={e => setGame(e.target.value)} />
+                    Search for a Game:
+                <input className="searchInput" type="text" value={game} onChange={e => setGame(e.target.value)} />
                 </label>
                 <input type="submit" value="Submit" />
-        </form>
-      </div>
+            </form>
+            {gamelist.length ? (
+            
+            <List>
+                {gamelist.map(game => (
+
+                    <ListItem key={game.id}>
+                        <img src={game.cover}/>
+                        <strong>
+                            {game.name}
+                        </strong>
+                    </ListItem>
+
+                ))}
+
+            </List>): ""}
+
+        </div>
 
     )
 
--- gamecollectcomplete/src/utils/API.js
+++ gamecollectcomplete/src/utils/API.js
@@ -1,30 +1,36 @@
 import axios from "axios";
 
-const languages = ["JavaScript", "Python", "C", "Ruby", "Java", "PHP", "C#"];
-// Export an object containing methods we'll use for accessing the random user API
 export default {
-  getUsersByLanguage: function(language) {
-    return new Promise((resolve, reject) => {
-      axios
-        .get("https://api.github.com/orgs/github/public_members")
-        .then(res => {
-          const users = res.data;
-          const results = users.map(user => {
-            return {
-              login: user.login,
-              image: user.avatar_url,
-              language: language
-            };
-          });
-          resolve(results);
-        })
-        .catch(err => reject(err));
-    });
+  searchGame: function (game) {
+    return axios({
+      url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games",
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'user-key': 'be68224bb6f48f6161aabc3fe5aaaf63',
+      },
+      data: `search "${game}";fields age_ratings,cover,first_release_date,name;`
+    })
   },
-  // Return a Promise to simulate an async call
-  getLanguagesList: function() {
-    return new Promise(resolve => {
-      resolve(languages);
-    });
+
+  findGameCover: function (id) {
+    return axios({
+      url: "https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/covers",
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'user-key': 'be68224bb6f48f6161aabc3fe5aaaf63'
+      },
+      data: `fields url;
+      where id=${id};`
+    })
+      .then(response => {
+        return response.data[0].url;
+      })
+      .catch(err => {
+        console.error(err);
+      })
+
   }
+
 };
